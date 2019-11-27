4f9fda95572c7016f137e1f0fa2a51c575ba19bf
Daria Szubierajska <d.szubierajska@gmail.com>
2019-11-27 12:48:14 -0300

    Private booking_params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,11 +5,18 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(cocktail_params)
+    @booking = Booking.new(booking_params)
     if @booking.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to kitchens_path(@kitchen)
     else
       render "new"
     end
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_at, :end_at)
+  end
+
 end
