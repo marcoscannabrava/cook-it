d305d13aac4e898ec152d35700f343474761a286
Manoel Tabet <manoelt@msn.com>
2019-11-27 21:29:03 -0300

    Kitchen New and Creating depending User

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -6,4 +6,20 @@ class KitchensController < ApplicationController
   def show
     @kitchen = Kitchen.find(params[:id])
   end
+
+  def new
+    @kitchen = Kitchen.new
+  end
+
+  def create
+    @kitchen = Kitchen.new(kitchen_params)
+    @kitchen.save
+    redirect_to kitchen_path(@kitchen)
+  end
+
+  private
+
+  def kitchen_params
+    params.require(:kitchen).permit(:title, :city, :description, :price_per_hour, :photo, :photo_cache)
+  end
 end
