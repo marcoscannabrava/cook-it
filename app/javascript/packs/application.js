96b420233b724bd4ec8a8d6acf4d534ba3e03440
Marcos Cannabrava <mpcannabrava@gmail.com>
2019-11-29 16:44:58 -0300

    implement Algolia Places API

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,8 @@ import "bootstrap";
 import "../plugins/flatpickr";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_algolia_places';
 
 initMapbox();
+
+initAutocomplete();
